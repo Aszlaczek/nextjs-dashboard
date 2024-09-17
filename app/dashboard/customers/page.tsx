aa350fe96eb6d034af796880af5cbeec3e411d56
Aszlaczek <awzorek23@wp.pl>
2024-09-17 13:01:23 +0200

    Final project

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Customers",
+  description: "Customers site",
+};
+
 export default function Page() {
   return <p>Customers Page</p>;
 }
